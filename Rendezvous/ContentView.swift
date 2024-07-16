675287cd986f05f9acba93502545634f5e768b38
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-16 11:26:47 -0400

    onTap on Map

--- Rendezvous/ContentView.swift
+++ Rendezvous/ContentView.swift
@@ -15,55 +15,33 @@ struct Location: Identifiable {
 }
 
 struct ContentView: View {
-   
+    
     @State private var position = MapCameraPosition.region(
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
             span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-            )
+        )
     )
     
     let locations = [
         Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-            Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
     ]
     var body: some View {
-        VStack {
-            MapReader { proxy in
-                Map {
-                    ForEach(locations) { location in
-                        Annotation(location.name, coordinate: location.coordinate) {
-                            Text(location.name)
-                                .font(.headline)
-                                .padding()
-                                .background(.blue)
-                                .foregroundStyle(.white)
-                                .clipShape(.capsule)
+        
+        MapReader { proxy in
+            Map()
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            print(coordinate)
                         }
-                        .annotationTitles(.hidden)
                     }
-                }
-            }
+                    
+                
             
-            HStack(spacing: 50) {
-                Button("Paris") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-
-                Button("Tokyo") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-            }
+            
+            
+            
         }
     }
 }
