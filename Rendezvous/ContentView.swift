1f72397c7c9757eafaa66aa62eb816716352a7cf
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-16 10:20:59 -0400

    conformance to Comparable for custom types

--- Rendezvous/ContentView.swift
+++ Rendezvous/ContentView.swift
@@ -6,16 +6,26 @@
 //
 
 import SwiftUI
-
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+    
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
 struct ContentView: View {
+    let users = [
+            User(firstName: "Arnold", lastName: "Rimmer"),
+            User(firstName: "Kristine", lastName: "Kochanski"),
+            User(firstName: "David", lastName: "Lister"),
+    ].sorted()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        List(users) { user in
+                    Text("\(user.lastName), \(user.firstName)")
+                }
     }
 }
 
