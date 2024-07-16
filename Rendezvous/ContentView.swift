5de2a0cc3fe1b47fbd51fbd1c6e42b008de1d586
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-16 12:15:15 -0400

    Add user locations to map

--- Rendezvous/ContentView.swift
+++ Rendezvous/ContentView.swift
@@ -23,17 +23,18 @@ struct ContentView: View {
     
     var body: some View {
         MapReader { proxy in
-            Map(initialPosition: startPosition){
+            Map(initialPosition: startPosition) {
                 ForEach(locations) { location in
-                    Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
-                }
-            }
-            .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.latitude)
-                        locations.append(newLocation)
+                        Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
                     }
-                    
+            }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                            let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                            locations.append(newLocation)
+                        
+                        print("Tapped at \(coordinate)")
+                    }
                 }
         }
     }
