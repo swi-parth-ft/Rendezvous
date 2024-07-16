07676f3ab50bf2116fbc1def4d6e266ec09af8af
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-16 10:10:07 -0400

    Initial Commit

---
+++ Rendezvous/RendezvousApp.swift
@@ -0,0 +1,17 @@
+//
+//  RendezvousApp.swift
+//  Rendezvous
+//
+//  Created by Parth Antala on 2024-07-16.
+//
+
+import SwiftUI
+
+@main
+struct RendezvousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
